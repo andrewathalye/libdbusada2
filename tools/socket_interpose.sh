b8cd904a20d1771c981f8de6d9dc370f632891d0
Andrew Athalye <andrewathalye@outlook.com>
2026-08-07 10:29:31 -0400

    Still working on empty array issue with a{sv}

--- tools/socket_interpose.sh
+++ tools/socket_interpose.sh
@@ -4,9 +4,10 @@
 ####################
 # Prints all data that would go to D-Bus Socket and redirects it through xd
 
-rm interpose.sock todbus.dat fromdbus.dat
+rm -r dat>/dev/null
+mkdir dat
 echo "Create socket"
-socat -r todbus.dat -R fromdbus.dat UNIX-LISTEN:interpose.sock UNIX-CONNECT:/run/user/$UID/bus &
+socat -r dat/todbus.dat -R dat/fromdbus.dat UNIX-LISTEN:dat/interpose.sock UNIX-CONNECT:/run/user/$UID/bus &
 sleep 0.25
-echo "Run command. Find data in todbus.dat and fromdbus.dat."
-DBUS_SESSION_BUS_ADDRESS=unix:path=$PWD/interpose.sock "$@"
+echo "Run command. Find data in dat/"
+DBUS_SESSION_BUS_ADDRESS=unix:path=$PWD/dat/interpose.sock "$@"
